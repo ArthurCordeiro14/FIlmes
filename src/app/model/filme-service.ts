73fe290a1d702980f2bdbc371b31372da4f82eb0
Arthur Cordeiro de Lima <20221144010035@ifrn.local>
2025-10-07 15:54:06 -0300

    segundo commit

--- src/app/model/filme-service.ts
+++ src/app/model/filme-service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable,inject } from '@angular/core';
+import { Filme } from './filme';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
   
+
+  private http = inject(HttpClient)
+
+
+
+  obterFilme(titulo:string):Observable<Filme>{
+    return this.http.get<Filme>(`https://www.omdbapi.com/?t=${titulo}&apikey=c33cae24`)
+  }
+
 }
